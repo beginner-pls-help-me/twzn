dc967edae7cf05fdd4319db13c585f8551abe940
beginner-pls-help-me <dthoa551@gmail.com>
2025-04-11 11:12:08 +0700

    main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/beginner-pls-help-me/twzn/main/fix%20lag.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/beginner-pls-help-me/twzn/main/arisecrossoverHoa.lua"))()
+
 
